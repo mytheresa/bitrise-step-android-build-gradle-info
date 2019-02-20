c37d7a9c937e4ffc5ae5eab44564c6948aea12a8
Vittorio Morganti <vittorio.morganti@mytheresa.com>
2019-02-20 15:38:52 +0100

    Create logic to read build.gradle file

--- step.sh
+++ step.sh
@@ -5,29 +5,26 @@ set -e
 
 #
 # Required parameters
-if [ -z "${manifest_file}" ] ; then
-  echo " [!] Missing required input: manifest_file"
+if [ -z "${build_gradle_file}" ] ; then
+  echo " [!] Missing required input: build_gradle_file"
   exit 1
 fi
-if [ ! -f "${manifest_file}" ] ; then
-  echo " [!] File doesn't exist at specified AndroidManifest.xml path: ${manifest_file}"
+if [ ! -f "${build_gradle_file}" ] ; then
+  echo " [!] File doesn't exist at specified build.gradle path: ${build_gradle_file}"
   exit 1
 fi
 
 # ---------------------
 # --- Configs:
 
-echo " (i) Provided Android Manifest path: ${manifest_file}"
+echo " (i) Provided build.gradle path: ${build_gradle_file}"
 echo
 
 # ---------------------
 # --- Main
 
-VERSIONCODE=`grep versionCode ${manifest_file} | sed 's/.*versionCode="//;s/".*//'`
-VERSIONNAME=`grep versionName ${manifest_file} | sed 's/.*versionName\s*=\s*\"\([^\"]*\)\".*/\1/g'`
-PACKAGENAME=`grep package ${manifest_file} | sed 's/.*package\s*=\s*\"\([^\"]*\)\".*/\1/g'`
-MINSDKVERSION=`grep minSdkVersion ${manifest_file} | sed 's/.*minSdkVersion="//;s/".*//'`
-TARGETSDKVERSION=`grep targetSdkVersion ${manifest_file} | sed 's/.*targetSdkVersion="//;s/".*//'`
+VERSIONCODE=`grep 'versionCode ' ${build_gradle_file} | sed 's/ *//' | cut -d' ' -f2`
+VERSIONNAME=`grep 'versionName ' ${build_gradle_file} | sed 's/ *//' | cut -d' ' -f2 | cut -d'"' -f2`
 
 if [ -z "${VERSIONCODE}" ] ; then
   echo " [!] Could not find version code!"
@@ -44,25 +41,3 @@ fi
 
 envman add --key AMI_VERSION_NAME --value "${VERSIONNAME}"
 echo " (i) Version Name: ${VERSIONNAME} -> Saved to \$AMI_VERSION_NAME environment variable."
-
-if [ -z "${PACKAGENAME}" ] ; then
-  echo " [!] Could not find package name!"
-  exit 1
-fi
-
-envman add --key AMI_PACKAGE_NAME --value "${PACKAGENAME}"
-echo " (i) Package Name: ${PACKAGENAME} -> Saved to \$AMI_PACKAGE_NAME environment variable."
-
-if [ -z "${MINSDKVERSION}" ] ; then
-  echo " No minimum SDK version found in manifest"
-else
-  envman add --key AMI_MIN_SDK_VERSION --value "${MINSDKVERSION}"
-  echo " (i) Minimum SDK version: ${MINSDKVERSION} -> Saved to \$AMI_MIN_SDK_VERSION environment variable."
-fi
-
-if [ -z "${TARGETSDKVERSION}" ] ; then
-  echo " No target SDK version found in manifest"
-else
-  envman add --key AMI_TARGET_SDK_VERSION --value "${TARGETSDKVERSION}"
-  echo " (i) Target SDK version: ${TARGETSDKVERSION} -> Saved to \$AMI_TARGET_SDK_VERSION environment variable."
-fi
